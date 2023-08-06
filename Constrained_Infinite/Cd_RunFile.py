21285a7ec2814dd5a9b04fefdc60b97bd158b426
tt386 <tt386@exeter.ac.uk>
2023-08-06 13:15:33 +0100

    remove extra runfile

--- Constrained_Infinite/Cd_RunFile.py
+++
@@ -1,8 +0,0 @@
-mport subprocess
-from Params import *
-
-for C in CList:
-    for d in dList:
-        subprocess.call(['python','RunFile.py','-C',str(C),'d',str(d)])
-        print("Finished C,d=",C,d)
-
